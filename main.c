1a8d06ccd0c11913d74621adc072b459e66dcd2f
shuichengyy5 <3331592296@qq.com>
2024-11-09 02:20:46 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
-//
-//
-//
-#include <stdio.h>
-int main()
-{
-    printf("Hello World");
-    return 0;
+//202410111303
+//3331592296@qq.com
+//余洋
+#include<stdio.h>
+
+int main(){
+	int n;
+	int i;
+	double sum=1.0;
+	
+	printf("输入一个正整数:");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++){
+		sum*=i;
+	}
+	
+	printf("n的阶乘=%f\n",sum);
+	
+	return 0;
 }
